16e032c414aa7d64840c27f6b6bce93530674d43
Immerzeel <kees.vanimmerzeel@sweco.nl>
2025-06-08 20:12:35 +0200

    brmath.r added

---
+++ R/brmath.r
@@ -0,0 +1,29 @@
+#' Base Functions for Analytical Solutions of Geohydrological Problems.
+#'
+#' Functions as defined in Part B ("Mathematical tools") in the book
+#' "Analytical solutions of geohydrological problems" by G.A. Bruggeman.
+#'
+#' @section Error functions:
+#'
+#' \code{\link{P}}
+#'
+#' \code{\link{Pconj}}
+#'
+#' \code{\link{dPdx}}
+#'
+#' \code{\link{dPdy}}
+#'
+#' \code{\link{dPconjdx}}
+#'
+#' \code{\link{dPconjdy}}
+#'
+#' \code{\link{R}}
+#'
+#' \code{\link{M}}
+#'
+#' @name brmath
+#'
+#' @importFrom pracma erfc
+#' @importFrom pracma erf
+#'
+NULL
